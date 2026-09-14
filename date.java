aa0bb6f115f9fb083737a87a846031184d714072
Msgbaker <1467004@learn.vsb.bc.ca>
2026-09-14 14:37:48 -0700

    Add files via upload

---
+++ date.java
@@ -0,0 +1,8 @@
+public class date {
+	public static void main(String[] arg){
+		int day=14;
+		String month="September ";
+		int year=2026;
+		System.out.println(month + day + ", " + year);
+	}
+}
---
+++ time.java
@@ -0,0 +1,8 @@
+public class time{
+	public static void main(String[] arg){
+		int hour=14;
+		int min=28;
+		int second = hour * 3600 + min * 60;
+		System.out.println(second + "s"); 
+	}
+}
