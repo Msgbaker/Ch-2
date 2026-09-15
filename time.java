312775848c12cec5e45472d0d9b0aad3d8256c6d
Msgbaker <1467004@learn.vsb.bc.ca>
2026-09-15 14:50:01 -0700

    Add files via upload

--- time.java
+++ time.java
@@ -3,6 +3,16 @@ public class time{
 		int hour=14;
 		int min=28;
 		int second = hour * 3600 + min * 60;
-		System.out.println(second + "s"); 
+		double seconds=second;
+		int timeToMidnight= 24 * 3600 - second;
+		double percentOfday= seconds/(24 * 3600)*100;
+		System.out.println("Time since midnight: " + second + "s"); 
+		System.out.println("Time till midnight: " + timeToMidnight + "s"); 
+		System.out.println("Precent of the day passed: " + percentOfday + "%");
+		hour=14;
+		min=43;
+		second= hour * 3600 + min * 60;
+		int timeElapsed= second - hour * 3600;
+		System.out.println("Time elapsed since class start " + timeElapsed + "s");
 	}
 }
